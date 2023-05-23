392a4d06f1dac64b8a855fe61d6a5641d9b35a27
Donnukrit Satirakul <osacn_donnukrit.satirakul@krungthai.com>
2023-05-24 03:19:28 +0700

    add unit test create wallet

---
+++ test/unit_test.dart
@@ -0,0 +1,17 @@
+import 'dart:developer';
+
+import 'package:finswallet/providers/wallet_provider.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  Provider.debugCheckInvalidValueType = null;
+  late WalletProvider walletProvider = WalletProvider();
+
+  group("WALLET TEST", () {
+    test('[CREATE WALLET]', () {
+      walletProvider.createWallet();
+      expect(walletProvider.ethKey, isNotNull);
+    });
+  });
+}
